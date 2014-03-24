a2529bae52a746e04b7f0c5a3d05fc48e3d1739b
Mauricio Soares <m.soares2603@gmail.com>
2014-03-24 18:53:21 -0300

    Init websockets in front

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,13 @@
 var app = app || {};
 
 (function() {
+    var url = location.protocol + '//' + location.hostname + ':' + location.port
+
     app.chat = function() {
         this.initialize();
     };
 
     app.chat.prototype.initialize = function() {
-        console.log('initing');
+        var socket = io.connect(url);
     };
 } ());
