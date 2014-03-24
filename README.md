a200c0be247b44b740dc998f20d1b5a0e06ba850
Mauricio Soares <m.soares2603@gmail.com>
2014-03-24 18:37:44 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple chat created with NodeJS and Socket.io
+
+That's just an experiment.
---
+++ app.js
@@ -0,0 +1,13 @@
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.engine('html', require('ejs').renderFile);
+
+    app.use(express.bodyParser());
+});
+
+
+server.listen(3333);
